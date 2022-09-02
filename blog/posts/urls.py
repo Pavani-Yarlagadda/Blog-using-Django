55faffa467c73ad732ab51e17186ab3d7c66eb2f
Pavani Yarlagadda <pavaniy777@gmail.com>
2022-09-02 12:08:30 +0530

    Delete blog directory

--- blog/posts/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.contrib import admin
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("",index,name='index'),
-    path('post/<str:pk>',post,name='post')
-]
